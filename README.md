e9f99ead8a0177998e7546ac0bc12bcc8e15b789
hgcho <hgcho@hgcho-work.mshome.net>
2019-07-28 15:15:34 -0400

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+1. git pull
+2. helm install --name hello-world ./helm
+3. kubectl get pod
+4. kubectl get svc
+5. curl http://192.168.99.100:32181
+
--- docker/hello.js
+++ docker/hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {  
     response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.write('Hello world with node 12.');
+    response.write('Hello world with node 12.\r\n');
     response.end();
 }).listen(8080);
 
