3416adf1a0ad38b3e77ede0608ef375e6aa94fbf
Hyungu Cho <hgcho@bluedigm.com>
2019-07-29 18:42:17 +0900

    fix hello.js

--- docker/hello.js
+++ docker/hello.js
@@ -1,10 +1,12 @@
 var http = require('http');
+var os = require('os');
 
-const hostname = process.env.HOSTNAME;
 
 http.createServer(function (request, response) {  
+    const hostname = process.env.HOSTNAME;
+
     response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.write('Hello world with node 12. hostname is [ ${hostname} ].\r\n');
+    response.write(`Hello world with node 12. hostname is [ ${hostname} ].\r\n`);
     response.end();
 }).listen(8080);
 
